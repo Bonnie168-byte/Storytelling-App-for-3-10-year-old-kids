55875086608b559d3a7838704a61e9e27e0e1a1a
Bonnie168-byte <xzhangjy@connect.ust.hk>
2026-05-08 16:46:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,22 +13,33 @@ def img2text(url):
 
 # text2story
 def text2story(text):
-    story_text = pipeline("text-generation", model="google/gemma-2-9b-it")
-    return story_text
+    story_pipe = pipeline("text-generation", model="roneneldan/TinyStories-1M")
+    prompt = (
+        f"Write a fun, simple and short story for 3-10 years old kids"
+        f"based on this scene: {scenario}. "
+        f"The story should be happy and easy to understand. Story:"
+    )
+    story_results = story_pipe(
+        prompt,
+        max_new_tokens=150,
+        num_return_sequences=1
+    )
+    story = story_results[0]["generated_text"]
+    return story
 
 # text2audio
 def text2audio(story_text):
-    audio_data = ""     # to be completed
+    audio_data = pipeline("text-to-audio", model="Matthijs/mms-tts-eng")
     return audio_data
 
 # Main part
 st.set_page_config(
-    page_title="Magic Story Time 🌈",
+    page_title="Kid's Story Session 🌈",
     page_icon="🧸",
     layout="centered"
 )
 st.header("Turn Your Image to Audio Story")
-uploaded_file = st.file_uploader("Select an Image...")
+uploaded_file = st.file_uploader("Select an Image...", type=["png", "jpg", "jpeg"])
 
 if uploaded_file is not None:
     # Save file locally
@@ -40,28 +51,17 @@ if uploaded_file is not None:
 
     # Stage 1: Image to Text (Using the function)
     st.text('Processing img2text...')
-    
-    image_to_text_model = pipeline("image-to-text", 
-                               model="Salesforce/blip-image-captioning-base")
-    scenario = image_to_text_model(uploaded_file.name)[0]["generated_text"]
-  
+    scenario = img2text(uploaded_file.name)
     st.write(f"**Scenario:** {scenario}")
 
     # Stage 2: Text to Story (Inline)
     st.text('Generating a story...')
-  
-    story_pipe = pipeline("text-generation", 
-                          model="roneneldan/TinyStories-1M")
-    story_results = story_pipe(scenario)
-    story = story_results[0]['generated_text']
-  
+    story = text2story(text)
     st.write(f"**Story:** {story}")
 
     # Stage 3: Story to Audio (Inline)
     st.text('Generating audio data...')
-    audio_pipe = pipeline("text-to-audio", 
-                          model="Matthijs/mms-tts-eng")
-    audio_data = audio_pipe(story)
+    audio_data = text2audio(story)
 
     # Play button
     if st.button("Play Audio"):
